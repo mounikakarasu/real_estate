b6399e5e2c963a5dab5019701a37d59ba543ca36
Mounika Karasu <mounikakarasu@gmail.com>
2026-06-22 04:38:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import os
 
 import aws_cdk as cdk
@@ -9,16 +8,14 @@ from nexus_crm.nexus_crm_stack import NexusCrmStack
 app = cdk.App()
 NexusCrmStack(app, "NexusCrmStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+    # Account/Region-dependent features and context lookups will not work, but a single synthesized template can be deployed anywhere.
 
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
     #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
+    # Uncomment the next line if you know exactly what Account and Region you want to deploy the stack to. */
 
     #env=cdk.Environment(account='123456789012', region='us-east-1'),
 
